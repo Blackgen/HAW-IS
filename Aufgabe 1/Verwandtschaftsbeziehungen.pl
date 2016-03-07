252c1f13de816f62c10a48a160d88648b44dd7da
Blackgen <jandb07@gmx.de>
2016-03-07 15:17:52 +0100

    Add few more realtionships to Aufgabe 1

--- Aufgabe 1/Verwandtschaftsbeziehungen.pl
+++ Aufgabe 1/Verwandtschaftsbeziehungen.pl
@@ -13,3 +13,32 @@ groﬂelternteil(X,Y) :- elternteil(X,Z), elternteil(Z,Y).
 
 % Opa: X ist Opa von Y
 opa(X,Y) :- mann(x), groﬂelternteil(X,Y).
+
+% Oma: X ist Oma von Y
+oma(X,Y) :- frau(X), groﬂelternteil(X,Y).
+
+
+% Vorfahre: X ist ein Vorfahre von Y
+vorfahre(X,Y) :- elternteil(X,Y).
+vorfahre(X,Y) :- elternteil(X,Z), vorfahre(Z,Y).
+
+
+% Geschwister: X ist Geschwisterteil von Y, gleiche Mutter & Vater
+geschwister(X,Y) :- vater(V,X), vater(V,Y),
+                    mutter(M,X), mutter(M,Y), X \= Y.
+                    
+% Halbgeschwister: Gleicher Vater oder gleiche Mutter
+halbgeschwister(X,Y) :- vater(V,X), vater(V,Y), X \= Y, not(geschwister(X,Y)).
+halbgeschwister(X,Y) :- mutter(M,X), mutter(M,Y), X \= Y, not(geschwister(X,Y)).
+
+% Bruder: X ist Bruder von Y
+bruder(X,Y) :- geschwister(X,Y), mann(X).
+
+% Schwester: X ist Schwester von Y
+schwester(X,Y) :- geschwister(X,Y), frau(X).
+
+% Onkel: X ist Onkel von Y
+onkel(X,Y) :- elternteil(Z,Y), bruder(X,Z).
+
+% Tante: X ist Tante von Y
+tante(X,Y) :- elternteil(X,Y), schwester(X,Z).
