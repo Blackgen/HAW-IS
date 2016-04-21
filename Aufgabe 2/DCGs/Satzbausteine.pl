da679c7ec994ffb2ad3123c96a8eba372c0ce55f
Patrick Steinhauer <patrick.steinhauer@haw-hamburg.de>
2016-04-21 13:41:39 +0200

    Finish questions with start: verb, iterogativ.

--- Aufgabe 2/DCGs/Satzbausteine.pl
+++ Aufgabe 2/DCGs/Satzbausteine.pl
@@ -1,42 +1,6 @@
 % Autor:
 % Datum: 24.03.2016
 
-%lex(wortart,wort)
-%lex(wortart,wort,abart,abartart)
-
-lex(verb,geht).
-
-lex(art,der).
-lex(art,die).
-lex(art,das).
-lex(art,dem).
-lex(art,den).
-
-lex(frage,wer).
-lex(frage,wie).
-lex(frage,was).
-
-lex(nomen,vater).
-lex(nomen,mutter).
-lex(nomen,onkel).
-lex(nomen,tante).
-lex(nomen,neffe).
-lex(nomen,nichte).
-lex(nomen,sohn).
-lex(nomen,tochter).
-lex(nomen,opa).
-lex(nomen,oma).
-lex(nomen,vorfahre).
-lex(nomen,geschwister).
-lex(nomen,halbgeschwister).
-lex(nomen,cousin).
-lex(nomen,cousine).
-
-
-lex(verb,ist).
-
-lex(pre,von).
-
 % ------------------------------------------------------------------------------
 
 % Interogativpronomen
@@ -45,35 +9,36 @@ lexi(wie, iterogativpronomen).
 lexi(was, iterogativpronomen).
 
 % Verben
-lexi(ist, ist, verb, singular).
+lexi(ist, ist, verb, mannlich).
+lexi(ist, ist, verb, weiblich).
 lexi(ist, sind, verb, plural).
-lexi(hat, hat, verb, singular).
+%lexi(hat, hat, verb, singular).
 
 % Praepositionen
 lexi(von, praeposition).
 
 % Artikel
-lexi(der, artikel).
-lexi(die, artikel).
-lexi(die, artikel).
+lexi(der, artikel, mannlich).
+lexi(die, artikel, weiblich).
+lexi(die, artikel, plural).
 
 % Nomen Singular
-lexi(onkel, onkel, nomen, singular).
-lexi(tante, tante, nomen, singular).
-lexi(opa, opa, nomen, singular).
-lexi(oma, oma, nomen, singular).
-lexi(vater, vater, nomen, singular).
-lexi(mutter, mutter, nomen, singular).
-lexi(schwester, schwester, nomen, singular).
-lexi(bruder, bruder, nomen, singular).
-lexi(halbschwester, halbschwester, nomen, singular).
-lexi(halbbruder, halbbruder, nomen, singular).
-lexi(uroma, uroma, nomen, singular).
-lexi(uropa, uroma, nomen, singular).
-lexi(cousin, cousin, nomen, singular).
-lexi(cousine, cousine, nomen, singular).
-lexi(nichte, nichte, nomen, singular).
-lexi(neffe, neffe, nomen, singular).
+lexi(onkel, onkel, nomen, mannlich).
+lexi(tante, tante, nomen, weiblich).
+lexi(opa, opa, nomen, mannlich).
+lexi(oma, oma, nomen, weiblich).
+lexi(vater, vater, nomen, mannlich).
+lexi(mutter, mutter, nomen, weiblich).
+lexi(schwester, schwester, nomen, weiblich).
+lexi(bruder, bruder, nomen, mannlich).
+lexi(halbschwester, halbschwester, nomen, weiblich).
+lexi(halbbruder, halbbruder, nomen, mannlich).
+lexi(uroma, uroma, nomen, weiblich).
+lexi(uropa, uroma, nomen, mannlich).
+lexi(cousin, cousin, nomen, mannlich).
+lexi(cousine, cousine, nomen, weiblich).
+lexi(nichte, nichte, nomen, weiblich).
+lexi(neffe, neffe, nomen, mannlich).
 
 % Nomen Plural
 % Vaeter und Muetter vielleicht nur wenn man von mehreren verschiedenen Kindern
--- Aufgabe 2/DCGs/SyntacticSugar.pl
+++ Aufgabe 2/DCGs/SyntacticSugar.pl
@@ -6,20 +6,23 @@
 :- consult('../../Aufgabe 1/Verwandtschaftsbeziehungen').
 :- consult('Satzbausteine').
 
-eingabe :- read_sentence(Satz), frage([Fun,Sub], Satz, ['.']),F =.. [Fun,X,Sub],F,write(X).
+eingabe :- read_sentence(Satz), frage([Fun,Sub], Satz, ['.']), F =.. [Fun,X,Sub],F,write(X).
 
 frage(Satz) --> nominalphrase(SatzNP, SingularPlural), verbalphrase(SatzVP, SingularPlural).
-frage(Satz) --> verbalphrase(SatzVP, SingularPlural), nominalphrase(SatzNP, SingularPlural).
+frage(Satz) --> verb(SatzVerb, SingularPlural), nominalphrase(SatzNP, SingularPlural), nominalphrase([SatzNP2, Rest], SingularPlural), {FF=.. [SatzNP2, X, Rest], FF, X == SatzNP}.
 frage(Satz) --> iterogativphrase(SatzIP, SingularPlural), nominalphrase(SatzNP, SingularPlural), praepositionalphrase(SatzPP, SingularPlural),
-                {Satz = [SatzNP,SatzPP]}.
+                {F=.. [SatzNP, X, SatzPP], findall(X,F,L), write(L)}.
 
 nominalphrase(SatzNP, _SingularPlural) --> eigenname(SatzNP).
-nominalphrase(SatzNP, SingularPlural) --> artikel, nomen(SatzNP, SingularPlural).
-nominalphrase(SatzNP, SingularPlural) --> artikel, nomen(SatzNP, SingularPlural), praepositionalphrase(SatzNP, SingularPlural).
+nominalphrase(SatzNP, SingularPlural) --> artikel(SingularPlural), nomen(SatzNP, SingularPlural).
+nominalphrase([SatzNP,SatzPP], SingularPlural) --> artikel(SingularPlural), nomen(SatzNP, SingularPlural), praepositionalphrase(SatzPP, SingularPlural).
+
 praepositionalphrase(SatzPP, SingularPlural) --> praeposition, nominalphrase(SatzPP, SingularPlural).
 %praepositionalphrase(SatzPP, SingularPlural) --> praeposition, eigenname(SatzPP), artikel, nomen(SatzPP, SingularPlural).
+
 verbalphrase(SatzVP, SingularPlural) --> verb(SatzVP, SingularPlural).
 verbalphrase(SatzVP, SingularPlural) --> verb(SatzVP, SingularPlural), nominalphrase(SatzVP, SingularPlural).
+
 iterogativphrase(Semantik, SingularPlural)  --> iterogativpronomen, verb(Semantik, SingularPlural) .
 
 eigenname(X)                                  --> [X], {mann(X)}.
@@ -28,4 +31,4 @@ nomen(Semantik, SingularPlural)               --> [X], {lexi(Semantik, X, nomen,
 praeposition                                  --> [X], {lexi(X, praeposition)}.
 verb(Semantik, SingularPlural)                --> [X], {lexi(Semantik, X, verb, SingularPlural)}.
 iterogativpronomen                            --> [X], {lexi(X, iterogativpronomen)}.
-artikel                                       --> [X], {lexi(X, artikel)}.
+artikel(Geschlecht)                           --> [X], {lexi(X, artikel,Geschlecht)}.
